66ec9456fd680973f3e216119024439ca8fa153a
aycabta <aycabta@gmail.com>
2018-12-28 21:21:26 +0900

    Revert "Use TCPServer instead of Sinatra"
    This reverts commit 91f0f35a88ec7dea01f2b7536fe488fad42caa58.

--- web.rb
+++ web.rb
@@ -1,42 +1,21 @@
 require 'bundler'
-require 'socket'
-require 'optparse'
+require 'sinatra'
 require './model'
 
-params = ARGV.getopts('p:e:')
-
-case params['e']
-when 'production'
+configure :production do
   DataMapper.setup(:default, ENV['DATABASE_URL'])
   database_upgrade!
-when 'test', 'development', nil
+end
+
+configure :test, :development do
   if ENV['DATABASE_URL']
-    # 'postgres://aycabta@localhost:5432/osieru-teacher'
     DataMapper.setup(:default, ENV['DATABASE_URL'])
   else
     DataMapper.setup(:default, 'yaml:///tmp/osieru-teacher')
   end
   database_upgrade!
-else
-  DataMapper.setup(:default, 'yaml:///tmp/osieru-teacher')
-  database_upgrade!
 end
 
-server = TCPServer.new(params['p'].to_i)
-loop do
-  body = 'osiete...'
-  client = server.accept
-  headers = []
-  while header = client.gets
-    break if header.chomp.empty?
-    headers << header.chomp
-  end
-  p headers
-
-  client.puts 'HTTP/1.0 200 OK'
-  client.puts 'Content-Type: text/html;charset=utf-8'
-  client.puts "Content-Length: #{body.bytesize}"
-  client.puts
-  client.puts body
-  client.close
+get '/' do
+  'osiete...'
 end
